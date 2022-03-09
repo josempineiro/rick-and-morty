990c55377ad4002cd2f5c9f6529caeee744142a0
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-03-09 21:02:48 +0000

    Fix page styles

--- components/ui/Page/Page.tsx
+++ components/ui/Page/Page.tsx
@@ -56,7 +56,7 @@ const Page = ({ title, children, breadcrumbs }: PageProps) => {
       </div>
 
       <main className="flex-1">
-        <div className="max-w-7xl mx-auto p-4 sm:px-6 md:px-8 flex h-full">
+        <div className="max-w-7xl mx-auto p-4 sm:px-6 md:px-8 flex flex-col h-full">
           {children}
         </div>
       </main>
