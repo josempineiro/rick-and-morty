5e2c75c7893963814f53a904643fb0ab5b5239a7
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-03-15 20:13:01 +0000

    Add meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,8 +47,50 @@ function App({ Component, pageProps }: Props) {
             href="/icons/safari-pinned-tab.svg"
             color="#5bbad5"
           />
-          <meta name="msapplication-TileColor" content="#1a1f18"></meta>
-          <meta name="theme-color" content="#192d1a"></meta>
+          <meta name="msapplication-TileColor" content="#0891b2"></meta>
+          <meta name="theme-color" content="#0891b2"></meta>
+          <meta name="title" content="Rick and Morty" />
+          <meta name="description" content="Funny Rick and Morty application" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="msapplication-TileColor" content="#0891b2" />
+          <meta
+            name="msapplication-TileImage"
+            content="/icons/android-chrome-512x512.png"
+          />
+          <meta name="theme-color" content="#0891b2" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta
+            name="google-site-verification"
+            content="12SzhX8l1tk3jBwLdqfOAbsJ-5DaB-AWcHKd7nJICI8"
+          />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://rick-and-morty.leman.dev/" />
+          <meta property="og:title" content="Rick and Morty" />
+          <meta
+            property="og:description"
+            content="Funny Rick and Morty application"
+          />
+          <meta
+            property="og:image"
+            content="/icons/android-chrome-512x512.png"
+          />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta
+            property="twitter:url"
+            content="https://rick-and-morty.leman.dev/"
+          />
+          <meta
+            property="twitter:title"
+            content="Funny Rick and Morty application"
+          />
+          <meta
+            property="twitter:description"
+            content="Funny Rick and Morty application"
+          />
+          <meta
+            property="twitter:image"
+            content="/icons/android-chrome-512x512.png"
+          />
         </Head>
         <Component {...pageProps} />
         <Signature />
