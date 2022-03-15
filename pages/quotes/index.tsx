7bf6460f94528624067af1e2c4a0299805c78d53
Jose Manuel Piñeiro Garcia <josempga@ext.inditex.com>
2022-03-15 20:30:06 +0000

    Add refresh quote button

--- pages/quotes/index.tsx
+++ pages/quotes/index.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import Loader from "components/ui/Loader";
 import Page from "components/ui/Page";
 import Quote from "components/quotes/Quote";
+import { RefreshIcon } from "@heroicons/react/outline";
+import Button from "components/ui/Button";
 
 const QuotesPage = () => {
+  const [state, setState] = useState(0);
   const [quote, setQuote] = useState(undefined);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(undefined);
@@ -18,7 +20,7 @@ const QuotesPage = () => {
       .finally(() => setLoading(false));
 
     // empty dependency array means this effect will only run once (like componentDidMount in classes)
-  }, []);
+  }, [state]);
 
   if (loading) {
     return <Loader variant="linear" />;
@@ -28,6 +30,14 @@ const QuotesPage = () => {
   return (
     <Page title={"¡Random quotes!"}>
       <Quote quote={quote} />
+      <div className="flex justify-center mt-8">
+        <Button
+          variant="clear"
+          size="huge"
+          icon={<RefreshIcon />}
+          onClick={() => setState(Math.random())}
+        />
+      </div>
     </Page>
   );
 };
